87680c93a32ff05f55fb44809494728815f50d11
ns011681 <ashishgolla2003@gmail.com>
2023-04-22 20:54:17 +0530

    store user details in database and displaying a alert message if user is created or already exists

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -7,7 +7,7 @@ function Register() {
     email:"",
     password:"",
   });
-  const register=async()=>{
+  const register = async()=>{
     try {
         const response = await axios.post('/api/users/register',user);
         if(response.data.success){
@@ -18,15 +18,15 @@ function Register() {
     } catch (error) {
         console.log(error);
     }
-  }
+  };
   return (
     <div className='min-h-screen flex items-center justify-center'>        
         <div className='flex flex-col gap-3 w-96 p-3 shadow border border-gray-300'>
             <h1 className='text-3xl font-bold text-gray-700'>WELCOME BACK</h1>
             <hr/>
             <input type='text' value={user.name} onChange={(e)=> setUser({...user,name: e.target.value})} placeholder='Name'/>
-            <input type='text' value={user.email} onChange={(e)=> setUser({...user,email: e.target.value})} placeholder='Email'/>
-            <input type='text' value={user.password} onChange={(e)=> setUser({...user,password: e.target.value})} placeholder='Password'/>
+            <input type='email' value={user.email} onChange={(e)=> setUser({...user,email: e.target.value})} placeholder='Email'/>
+            <input type='password' value={user.password} onChange={(e)=> setUser({...user,password: e.target.value})} placeholder='Password'/>
             <button type='submit' onClick={register} className='primary'>Register</button>
         </div>        
     </div>
--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 const dbConfig = require("./config/dbConfig");
-const port = 4000;
-app.use(express.json);
+const port = 5000;
+app.use(express.json());
 const userRoute = require("./routes/userRoute");
 
 app.use("/api/users", userRoute);
