cf63a4642c42c810d6faa65a5c9146a010a5a6a1
ns011681 <ashishgolla2003@gmail.com>
2023-04-22 18:01:12 +0530

    adding style to heading

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
-
-
 function App() {
   return (
     <div className="App">
-      <h1>Music player</h1>
+      <h1 className="text-center font-bold bg-slate-100 text-5xl p-5">
+        Music player
+      </h1>
     </div>
   );
 }
